157b8689e1b63ae6f13addfc8729e4527b4acd8f
Aleksandr <derevjanko9@gmail.com>
2021-12-21 20:25:34 +0300

    ДЗ 4

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -19,11 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import index, contacts
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts'),
     path('products/', include('mainapp.urls', namespace='products')),
+    path('auth/', include('authapp.urls', namespace='auth')),
 ]
 
 if settings.DEBUG:
