6f4d56e67cf6dcd2e5b2db4d2176cf48023f0f9c
Aleksandr <derevjanko9@gmail.com>
2021-12-26 09:58:00 +0300

    ДЗ 6

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,19 +1,36 @@
+import random
 from django.shortcuts import render, get_object_or_404
 from mainapp.models import ProductCategory
 from mainapp.models import Product
 from basketapp.models import Basket
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)
+
+    return same_products
+
+
 def products(request, pk=None):
     title = 'каталог'
     links_menu = ProductCategory.objects.all()
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-
-    basket_sum = 0
-    for item in Basket.objects.all():
-        basket_sum += item.quantity
+    basket = get_basket(request.user)
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+    products = Product.objects.all().order_by('price')
 
     if pk is not None:
         if pk == 0:
@@ -22,26 +39,37 @@ def products(request, pk=None):
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category__pk=pk).order_by('price')
-        product_1 = Product.objects.all()[2]
+
         context = {
             'title': title,
             'links_menu': links_menu,
-            'product_1': product_1,
             'products': products,
             'category': category,
             'basket': basket,
-            'basket_sum': basket_sum,
+            'hot_product': hot_product,
+            'same_products': same_products,
         }
         return render(request, 'mainapp/products.html', context=context)
 
-    same_products = Product.objects.all()
-    product_1 = Product.objects.all()[2]
     context = {
         'title': title,
         'links_menu': links_menu,
-        'product_1': product_1,
-        'same_products': same_products,
+        'products': products,
         'basket': basket,
-        'basket_sum': basket_sum,
+        'hot_product': hot_product,
+        'same_products': same_products,
     }
     return render(request, 'mainapp/products.html', context=context)
+
+
+def product(request, pk):
+    title = 'продукты'
+
+    content = {
+        'title': title,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'mainapp/product_detail.html', content)
